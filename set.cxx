0495a4b475b888ff58f3b1c548ff2418241afbba
ItayVolk <ivolk@scu.edu>
2025-10-10 20:08:53 -0700

    Implemented insert

--- set.cxx
+++ set.cxx
@@ -19,7 +19,6 @@ namespace csen79 {
     // assignment
     Set &Set::operator=(const Set &rhs) {
         memcpy(data, rhs.data, DATASIZE);
-        first = rhs.first;
         count = rhs.count;
         return *this;
     }
@@ -40,32 +39,43 @@ namespace csen79 {
     }
 
 
-    //Add data to end of Q
-    void Set::enQ(const Data &element) {
-        //Check if adding doesn't surpass limits
-        if (count >= DATASIZE){
-            throw std::out_of_range("The Queue is Full. DeQ Items to Add More.");
+    //Add data to set if not inside
+    void Set::insert(const Data &element) {
+        //Check if data isn't in set
+        for (int i = 0; i < count; i++) {
+            if (data[i] == element) {
+                return;
+            }
         }
 
-        //Set data at the end
-        data[(first+count)%DATASIZE] = element;
+        //Increase size if necessary
+        if (count == DATASIZE) {
+            DATASIZE += 200;
+            if (data != nullptr) {
+                Data* temp = new Data[DATASIZE];
+                memcpy(temp, data, count*sizeof(Data));
+                delete data;
+                data = temp;
+            } else {
+                data = new Data[DATASIZE];
+            }
+        }
 
-        //Increment the count for every add
-        count++;
+        data[count++] = element; //Insert data and increment
     };
     //Remove data from end of Q
     Set::Data Set::deQ() {
         if (count == 0)
             throw std::out_of_range(std::string("queue underflow"));
-        Data value = data[first];
-        first = (first+1)%DATASIZE;
+        Data value = data[0];
+        // first = (first+1)%DATASIZE;
         count--;
         return value;
     };
     //Prints the data to cout
     void Set::print() const {
         for (int i = 0; i < count; i++) {
-            std::cout << data[(first + i) % DATASIZE] << " ";
+            std::cout << data[i] << " ";
         }
         std::cout << std::endl;
     };
--- set.h
+++ set.h
@@ -11,10 +11,9 @@
 namespace csen79 {
 
 class Set {
-    static constexpr size_t DATASIZE = 1000;    // array size
 public:
     using Data = int;   // create an alias for the data
-    Set(): data{0}, first{0}, count{0} {}
+    Set(): data{0}, count{0} {}
 
     // rule of 5
     ~Set() = default;
@@ -24,15 +23,15 @@ public:
     Set &operator=(Set &&rhs);
 
     // access functions
-    void enQ(const Data &);
+    void insert(const Data &);
     Data deQ();
     void print() const;
 
 private:
     // data storage
-    int first;
     int count;
-    Data data[DATASIZE];
+    size_t DATASIZE;    // array size
+    Data* data;
 };
 
 }
